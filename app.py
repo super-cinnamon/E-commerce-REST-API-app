44c4f9d129b0814ff4460c8835db8f99387224e3
Syrina-Akai <65730591+Syrina-Akai@users.noreply.github.com>
2022-05-24 14:14:00 +0100

    organized

---
+++ app.py
@@ -0,0 +1,57 @@
+from PyQt5 import QtWidgets, uic, QtGui,QtCore
+import sys
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('mainWindow.ui', self)
+
+        #the list of products
+        self.product_list = self.findChild(QtWidgets.QListView,"product_list")
+        self.product_list.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
+        self.listModel = QtGui.QStandardItemModel()
+        self.product_list.setModel(self.listModel)
+        self.product_list.clicked[QtCore.QModelIndex].connect(self.openSelectedClickListener)
+        item = QtGui.QStandardItem(f'test')
+        self.listModel.appendRow(item)
+        
+        #product image
+        self.product_image=self.findChild(QtWidgets.QLabel,"product_image")
+        self.product_image.setPixmap(QtGui.QPixmap("./images/10.png"))
+
+        #description of the product
+        self.product_desc=self.findChild(QtWidgets.QTextEdit,"product_desc")
+        self.product_desc.setReadOnly(True)
+
+        #the name of the product
+        self.product_name = self.findChild(QtWidgets.QLabel,"product_name")
+        
+        #id of the bidder
+        self.bidder_id = self.findChild(QtWidgets.QTextEdit,"bidder_id")
+        
+        #name of the bidder
+        self.bidder_name = self.findChild(QtWidgets.QTextEdit,"bidder_name")
+        
+        #the suggested price 
+        self.bidder_price = self.findChild(QtWidgets.QSpinBox,"bidder_price")
+        
+        #button to validate the bid
+        self.bid_validation = self.findChild(QtWidgets.QPushButton,"bid_validation")
+
+        #liste of bidders
+        self.bidders_list = self.findChild(QtWidgets.QTableView,"bidders_list")
+
+        self.show()
+
+    def openSelectedClickListener(self, index):
+        item = self.listModel.itemFromIndex(index)
+        self.product_name.setText(item.text())
+        ## update all the info from the database
+        #get requests loop
+        
+
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
