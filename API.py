6ab564bb26f4396c600c98a1455e11acbdce8157
Syrina-Akai <65730591+Syrina-Akai@users.noreply.github.com>
2022-06-16 11:18:47 +0100

    api finished

--- API.py
+++ API.py
@@ -13,6 +13,9 @@ api = Api(app)
 item_put_args = reqparse.RequestParser()
 item_put_args.add_argument("owner", type=str, help="Name of the winning bidder", location='form')
 item_put_args.add_argument("prix_achat", type=int, help="Price item was bought for", location='form')
+item_put_args.add_argument("name", type=str, help="Name of the item", location='form')
+item_put_args.add_argument("description", type=str, help="Description of the item", location='form')
+item_put_args.add_argument("estimation", type=str, help="Estimation of the item's price", location='form')
 
 class Auction(Resource):
         def get(self, ID):
@@ -51,10 +54,48 @@ class Auction(Resource):
                                 db.seek(0)
                                 db.write(updt)
                                 db.truncate()
-                        
-                        
+                else: abort(404, message="Could not find item with that id")
+
+        def delete(self,ID):
+                #database
+                with open("data.json", "r+",encoding="utf-8") as db : 
+                       database = json.load(db)
+                db.close()    
+                idObj = f'{ID}'
+                #we delete the item
+                if idObj in database.keys():
+                        database.pop(idObj)
+                        #database
+                        with open("data.json", "r+",encoding="utf-8") as db : 
+                                updt = json.dumps(database, indent=4, ensure_ascii=False)
+                                db.seek(0)
+                                db.write(updt)
+                                db.truncate()
                 else: abort(404, message="Could not find item with that id")
                 
+        def post(self,ID):
+                #database
+                with open("data.json", "r+",encoding="utf-8") as db : 
+                       database = json.load(db)
+                db.close() 
+                args = item_put_args.parse_args()
+                item = {"Name": f"{args['name']}",
+                        "Description": f"{args['description']}",
+                        "Estimation": f"{args['estimation']}",
+                        "Acheteur":  f"{args['owner']}",
+                        "Prix d'achat": f"{args['prix_achat']}"}
+                idObj = f'{ID}'
+                if idObj not in database.keys():
+                        database[f"{idObj}"]=item
+                        with open("data.json", "r+",encoding="utf-8") as db : 
+                                        updt = json.dumps(database, indent=4, ensure_ascii=False)
+                                        db.seek(0)
+                                        db.write(updt)
+                                        db.truncate()
+                else :  abort(409, message="Diri Conflict detected, this element already exists") 
+                
+                
+
 
 api.add_resource(Auction, "/auction/<int:ID>")
 
--- test.py
+++ test.py
@@ -2,8 +2,14 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "auction/0", {})
 
+item = {"Name": "Pend",
+        "Description": "Pend",
+        "Estimation": "22900-30500",
+        "Acheteur":  "NONE",
+        "Prix d'achat": 0}
+#response = requests.delete(BASE + "auction/1")
+response = requests.post(BASE + f"auction/11",{"name":"Table vintage", "description":"Table en fer avec les têtes du Britannie", "estimation":"124000-130000","owner": "NONE", "prix_achat": 0})
 print(response.json())
 
 #response = requests.get(BASE + "auction/3")
